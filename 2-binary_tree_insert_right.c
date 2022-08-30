e411523f4e31c577e4e56f32011a805f0f00736f
NekodaMushi <4252@holbertonschool.com>
2022-08-30 14:03:58 +0000

    Condition changed

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,17 +12,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = binary_tree_node(parent, value);
-
 	if (!parent)
 		return (NULL);
+	new_node = binary_tree_node(parent, value);
 
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
+		parent->right = new_node;
 	}
-	parent->right = new_node;
+	else
+		parent->right = new_node;
 
-	return (new_node);
+		return (new_node);
 }
