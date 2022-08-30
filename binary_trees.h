5979973eea1a37095e4f7e45102d1b92fe09f5aa
NehemieMbg <nehemie.mbg@gmail.com>
2022-08-30 11:40:46 +0000

    pre merge

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,6 +23,8 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s heap_t;
 
+/* Prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
-
-#endif /* 	BINARY_TREES_H */
+#endif /* BINARY_TREES_H */
