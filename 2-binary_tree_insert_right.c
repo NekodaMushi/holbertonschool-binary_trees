98ab667d7787e9c63d28105b5110111bb67ea29b
NekodaMushi <4252@holbertonschool.com>
2022-08-30 13:57:39 +0000

    Adding right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Fct that creates the right child to have
+ *
+ * @parent: upper node of tree
+ * @value: data stored inside node
+ *
+ * Return: New node created
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = binary_tree_node(parent, value);
+
+	if (!parent)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	else
+		parent->right = new_node;
+
+	return (new_node);
+}
