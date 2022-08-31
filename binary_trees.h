3cbed6c16a1b2e5cfaf5c94a701316d6fcc6f6e8
NekodaMushi <4252@holbertonschool.com>
2022-08-31 08:43:47 +0000

    Main updated

--- binary_trees.h
+++ binary_trees.h
@@ -42,5 +42,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif
