c926e34ebbf923d7c340901fa13fd330309e9aa3
NekodaMushi <4252@holbertonschool.com>
2022-09-01 10:54:51 +0000

    parent parent child is my uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Function that check if node
+ * has a uncle
+ *
+ * @node: is the pointer of node of the binary tree
+ *
+ * Return: (1) if tree is full , (0) otherwise.
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
