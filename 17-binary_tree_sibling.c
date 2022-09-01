cab829e41f272b6256977d4964ed064eb9096d9c
NekodaMushi <4252@holbertonschool.com>
2022-09-01 10:03:10 +0000

    Nice and clean

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full - Function that count nodes
- * with at least 1 child
+ * binary_tree_sibling - Function that check if node
+ * has a sibling
  *
- * @tree: is the pointer that point to the root of the binary tree
+ * @node: is the pointer of node of the binary tree
  *
  * Return: (1) if tree is full , (0) otherwise.
  */
@@ -25,3 +25,4 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 			return (node->parent->left);
 	}
 	return (NULL);
+}
